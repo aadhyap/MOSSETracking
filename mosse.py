7ae77f0393cde62a4b82768aabe1dc4b3f46baa8
aadhyaputtur <putturaadhya@gmail.com>
2022-10-11 12:30:55 -0400

    frames sorted, frame list created

--- mosse.py
+++ mosse.py
@@ -10,8 +10,12 @@ class MOSSE:
     def __init__(self, args):
 
         #this is where you make a list of all your video frames
+
+
+        
         videopath = args.videopath
         framepath = args.framepath
+        '''
         cap = cv2.VideoCapture(videopath)
         ret,frame = cap.read()
         count = 0
@@ -21,15 +25,38 @@ class MOSSE:
             ret,frame = cap.read()
             count += 1
 
+        '''
 
-    #def initialize():
+        #create a list for the frames
+        framelist = []
+
+
+        count = 0 
+        for frame in os.listdir(framepath):
+
+            path = framepath + 'frame%d.png' % count
+            framelist.append(path)
+            count +=1           
+        
+
+        self.framelist = framelist
+
+        '''
+
+        for i in range(len(self.framelist)):
+
+            img = cv2.imread(framelist[i])
+            #cv2.imshow("framelist[%d] " % i , img)
+            #cv2.waitKey(50)
+
+            '''
+
+        print("Data has already been initialized and read")
+
+
+    #def tracking():
 
 
 
         #Get first frame of video to get ROI and everything
-
-
         #crop that photo and start initializing 
-
-
-
